4b385765e676dd9c866c2085add74d6dd1783b17
markdunning <mark.dunning@gmail.com>
2020-01-14 16:00:31 +0000

    Add bioconductor repositories
    Add references and contacts
    
    Add option to download plots

--- app.R
+++ app.R
@@ -8,6 +8,8 @@
 #
 
 library(shiny)
+library(BiocManager)
+options(repos = BiocManager::repositories())
 library(devtools)
 #install_github("sheffield-bioinformatics-core/sbcMisc")
 library(ggbeeswarm)
@@ -29,11 +31,20 @@ ui <- navbarPage("Explore the single-cell data...",
          selectInput("sel_genes","Select your genes..",choices = anno$mgi_symbol,selected = "",selectize = TRUE,multiple = TRUE),
          checkboxInput("exclude_NA","Exclude Missing Values",value=TRUE),
          checkboxInput("flip_axis","Rotate Axis?",value=FALSE),
-         checkboxGroupInput(inputId = "plot_type",label="Type of Plot", choices=c("Boxplot","Points","Violin","Beeswarm"),selected="Boxplot"),
+         checkboxGroupInput(inputId = "plot_type",label="Type of Plot", choices=c("Boxplot","Points","Violin"),selected="Boxplot"),
         selectInput(inputId = "selected_clus",label="Select Clusters to Compare",0:17, multiple = TRUE,selectize = TRUE,selected=c(0,1,3:5,7:15,17)),
         selectInput("umap_type","Color UMAP by...",choices=c("expression","cluster"),selected = "expresesion"),
-        checkboxInput("include_type","Include Cell Type", value=TRUE),
-        selectInput("cell_types", "Choose Cell Types...",choices = unique(bcode_info$Cell.Group),selected = unique(bcode_info$Cell.Group), selectize = TRUE,multiple = TRUE)
+        checkboxInput("include_type","Breakdown into Cell Type", value=TRUE),
+        selectInput("cell_types", "Choose Cell Types...",choices = unique(bcode_info$Cell.Group),selected = unique(bcode_info$Cell.Group), selectize = TRUE,multiple = TRUE),
+        h2("Output options"),
+        textInput("profileBasename", label = "What to call the output files",value="boxplot"),
+        radioButtons("profilePlotFormat", "File format for plots", choices=c("pdf","png"), selected="pdf"),
+        helpText("PDF can be imported into Illustrator (or similar) for editing. PNG plots are suitable for presentation"),
+        helpText("PDF dimensions are measured in inches, and PNG dimensions are measured in pixels"),
+        textInput("profileWidth", label="Width of plot ",value = 1200),
+        textInput("profileHeight", label="Height of plot ",value=600),
+        downloadButton("dl_boxplot","Export current boxplot(s)...."),
+        downloadButton("dl_umap","Export current UMAP(s)....")
         # selectInput("boxplotTheme", "Pick a plot style",choices=c("bw","grey","classic","minimal","light","Wall Street Journal","Economist", "Excel","solarized","stata","calc","dark","fivethirtyeight","tufte"),selected="bw"),
         # selectInput("pal_name", "Choose a colour palette", choices = c("UOS","Set1","Set2","Set3","Pastel1","Pastel2","Paired","Dark2","Accent"),selected="UOS")
       ),
@@ -42,7 +53,12 @@ ui <- navbarPage("Explore the single-cell data...",
       mainPanel(
         tabsetPanel(
          tabPanel("Boxplots", plotOutput("gene_boxplot")),
-         tabPanel("UMAP",plotOutput("umap_plot"))
+         tabPanel("UMAP",plotOutput("umap_plot")),
+         tabPanel("About",h3("App developed by Sheffield Bioinformatics Core"),br(),
+                  img(src="logo-sm.png"), br(),a("sbc.shef.ac.uk",href="http://sbc.shef.ac.uk"),br(),
+                 helpText("For queries about the app and its usage, please contact Sheffield Bioinformatics Core"),br(),
+                 helpText("For queries about the data and its interpretation, please contact the study authors"),
+                 h2("Chen, P., Qin, L., Li, G. et al. Endothelial TGF-β signalling drives vascular inflammation and atherosclerosis. Nat Metab 1, 912–926 (2019) doi:10.1038/s42255-019-0102-3"))
         )
       )
       )
@@ -50,19 +66,87 @@ ui <- navbarPage("Explore the single-cell data...",
    )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output,session) {
   library(tidyverse)   
 
   counts <- readRDS("counts.rds")
   bcode_info <- readRDS("barcodes.rds")
   anno <- readRDS("anno.rds")
 
-  
+  observeEvent(input$profilePlotFormat,{
+    
+    if(input$profilePlotFormat == "pdf"){
+      updateTextInput(session, "profileWidth",value=12)
+      updateTextInput(session, "profileHeight",value=6.3)
+    } else{
+      updateTextInput(session, "profileWidth",value=1200)
+      updateTextInput(session, "profileHeight",value=600)
+      
+    }
+    
+  })
 
-  output$gene_tbl <- DT::renderDataTable(anno,options=list(pageLength=5))
-  
-   output$gene_boxplot <- renderPlot({
 
+  output$gene_boxplot <- renderImage({
+    
+    p1 <- prepareBoxplot()
+    width  <- session$clientData$output_gene_boxplot_width
+    height <- session$clientData$output_gene_boxplot_height
+    
+    nrows <- length(input$sel_genes)
+    
+    ### stop an error occurring if nothing is selected
+    
+    if(is.na(nrows) | nrows == 0) nrows <- 1
+    height <- height * nrows
+    
+    # For high-res displays, this will be greater than 1
+    pixelratio <- session$clientData$pixelratio
+    
+    # A temp file to save the output.
+    outfile <- tempfile(fileext='.png')
+    
+    # Generate the image file
+    png(outfile, width=width*pixelratio, height=height*pixelratio,
+        res=72*pixelratio)
+    
+    print(p1)
+    dev.off()
+    list(src = outfile,
+         width = width,
+         height = height,
+         alt = "This is alternate text")
+  }
+  )
+ 
+  output$dl_boxplot <- downloadHandler(
+    filename = function(){
+      paste0(input$profileBasename,".",input$profilePlotFormat)
+    },
+    content = function(file) {
+      
+      if(input$profilePlotFormat == "pdf") pdf(file, width=as.numeric(input$profileWidth), height=as.numeric(input$profileHeight))
+      else png(file, width=as.numeric(input$profileWidth),height=as.numeric(input$profileHeight))
+      
+      
+      
+      p1 <- prepareBoxplot()
+      
+      print(p1)
+      dev.off()
+    }
+    
+    
+  )
+  
+  
+  
+  
+#   output$gene_boxplot <- renderPlot({
+
+  
+    prepareBoxplot <- reactive({
+  
      p <- ggplot()
 
 #  sel_genes <- ifelse(is.null(input$gene_tbl_rows_selected),"ACTB", 
@@ -82,7 +166,7 @@ server <- function(input, output) {
 
       p <- data %>% 
       ggplot(aes(x=Cell.ClusterLabel,y=Expression,fill=factor(Cell.ClusterLabel),group=Cell.ClusterLabel)) + 
-        scale_fill_manual(values= c("0"=rgb(3,4,4,maxColorValue = 255),#0
+        scale_fill_manual("Cluster",values= c("0"=rgb(3,4,4,maxColorValue = 255),#0
                                     "1"=rgb(242,235,15, maxColorValue = 255),#1
                                     "2"=rgb(89,202,233,maxColorValue = 255),#2
                                     "3"=rgb(186,80,153,maxColorValue = 255),#3
@@ -128,7 +212,43 @@ server <- function(input, output) {
    })
    
    
-   output$umap_plot <- renderPlot({
+    
+    output$umap_plot <- renderImage({
+      
+      p1 <- prepareUmap()
+      width  <- session$clientData$output_gene_boxplot_width
+      height <- session$clientData$output_gene_boxplot_height
+      
+      nrows <- ceiling(length(input$sel_genes)/2)
+      
+      ### stop an error occurring if nothing is selected
+      
+      if(is.na(nrows) | nrows == 0) nrows <- 1
+      height <- height * nrows
+      
+      # For high-res displays, this will be greater than 1
+      pixelratio <- session$clientData$pixelratio
+      
+      # A temp file to save the output.
+      outfile <- tempfile(fileext='.png')
+      
+      # Generate the image file
+      png(outfile, width=width*pixelratio, height=height*pixelratio,
+          res=72*pixelratio)
+      
+      print(p1)
+      dev.off()
+      list(src = outfile,
+           width = width,
+           height = height,
+           alt = "This is alternate text")
+    }
+    )
+    
+    
+  prepareUmap <- reactive({  
+    
+##   output$umap_plot <- renderPlot({
      
      p <- ggplot()
      
@@ -186,6 +306,27 @@ server <- function(input, output) {
      p
    })
    
+  output$dl_umap <- downloadHandler(
+    filename = function(){
+      paste0(input$profileBasename,".",input$profilePlotFormat)
+    },
+    content = function(file) {
+      
+      if(input$profilePlotFormat == "pdf") pdf(file, width=as.numeric(input$profileWidth), height=as.numeric(input$profileHeight))
+      else png(file, width=as.numeric(input$profileWidth),height=as.numeric(input$profileHeight))
+      
+      
+      
+      p1 <- prepareUmap()
+      
+      print(p1)
+      dev.off()
+    }
+    
+    
+  )
+  
+  
 }
 
 # Run the application 
